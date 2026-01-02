21cd88e2ed80fb6bc2e497cf612ad4433bb41e83
annamariemickova <mickova2@spsejecna.cz>
2026-01-02 12:24:09 +0100

    feat: update room model with new parameters

--- StudyRoomReservation/Models/Room.cs
+++ StudyRoomReservation/Models/Room.cs
@@ -8,6 +8,8 @@ public class Room
     public string Name { get; set; } = String.Empty;
     public List<Seat> Seats { get; set; } = new List<Seat>();
     public int Capacity { get; set; }
+    public int? Floor { get; set; }
+    public List<Equipment> Equipment { get; set; } = new List<Equipment>();
 
     /// <summary>
     /// Initializes a new instance of Room class.
@@ -16,13 +18,15 @@ public class Room
     /// <param name="name">Name of the room</param>
     /// <param name="capacity">Maximum number of seats</param>
     /// <exception cref="ArgumentException"></exception>
-    public Room(string name, int capacity)
+    public Room(string name, int capacity, int floor)
     {
         if (string.IsNullOrWhiteSpace(name)) throw new ArgumentException("Room name cannot be empty.");
         if (capacity <= 0) throw new ArgumentException("Room capacity must be greater than zero.");
+        if (floor <= 0) throw new ArgumentException("Floor must be greater than zero.");
         
         Name = name;
         Capacity = capacity;
+        Floor = Floor;
 
         for (int i = 1; i <= capacity; i++)
         {
