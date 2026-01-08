600e09781a94e365a54d93ab9d5602b3f06d0c9b
annamariemickova <mickova2@spsejecna.cz>
2026-01-08 18:29:18 +0100

    docs: add documentation to class

--- StudyRoomReservation/Models/Equipment.cs
+++ StudyRoomReservation/Models/Equipment.cs
@@ -1,9 +1,19 @@
 namespace StudyRoomReservation;
 
+/// <summary>
+/// Model class representing equipment for room.
+/// </summary>
 public class Equipment
 {
     public int Id { get; set; }
     public string Name { get; set; } = string.Empty;
+    
+    public Equipment(){}
+
+    public Equipment(string name)
+    {
+        Name = name;
+    }
 }
 
 public class RoomEquipment
